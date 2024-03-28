84d7ccbe7ae9a4b7267734ec3716ef12250286a7
gitirana <thaygitirana@gmail.com>
2024-03-28 13:38:04 -0300

    feat: create Box stories

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,16 @@
+import { Box, BoxProps } from '@lions-ui/react'
+import { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'Form/Box',
+  component: Box,
+  args: {
+    children: (
+      <>
+        <span>Testando o elemento Box</span>
+      </>
+    ),
+  },
+} as Meta
+
+export const Primary: StoryObj<BoxProps> = {}
